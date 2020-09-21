5fc5ab0c001a3e6cb4ade51871c287b8d42ad089
ryans <ryans@jlab.org>
2020-09-21 10:43:33 -0400

    Initialize project

---
+++ src/test/java/org/jlab/kafka/connect/transforms/SubstituteTest.java
@@ -0,0 +1,10 @@
+package org.jlab.kafka.connect.transforms;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SubstituteTest {
+    @Test public void testSomeLibraryMethod() {
+        Substitute.Value classUnderTest = new Substitute.Value();
+    }
+}
