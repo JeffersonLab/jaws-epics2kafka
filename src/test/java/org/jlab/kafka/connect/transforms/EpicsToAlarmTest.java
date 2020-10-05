f23d8589a41214e6c17f3a7108cbd328f82530b0
ryans <ryans@jlab.org>
2020-10-05 16:16:53 -0400

    This test should really do something...

---
+++ src/test/java/org/jlab/kafka/connect/transforms/EpicsToAlarmTest.java
@@ -0,0 +1,15 @@
+package org.jlab.kafka.connect.transforms;
+
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EpicsToAlarmTest {
+    @Test public void testSomeLibraryMethod() {
+        EpicsToAlarm.Value classUnderTest = new EpicsToAlarm.Value();
+
+        //ConnectRecord record = new ConnectRecord();
+
+        //classUnderTest.operatingValue(record);
+    }
+}
